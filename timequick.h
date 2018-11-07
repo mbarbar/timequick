ac0c3040dd894cc593d03b9c8c49bf21779eb769
Mohamad Barbar <mbarbar@runbox.com>
2018-11-07 15:16:48 +1100

    Implement time_start and time_end.

--- timequick.h
+++ timequick.h
@@ -2,6 +2,9 @@
    Header only library to quickly time things.
  */
 
+#include <time.h>
+#include <stdio.h>
+
 #ifndef TIMEQUICK_H
 #define TIMEQUICK_H
 
@@ -13,6 +16,8 @@ enum tq_resolution {
         tq_NANOSECONDS
 };
 
+static struct timespec tq_start_time;
+
 /* Set the time resolution for printing. Only affects output. */
 static void tq_set_resolution(enum tq_resolution resolution) {
 
@@ -20,12 +25,19 @@ static void tq_set_resolution(enum tq_resolution resolution) {
 
 /* Start timing. */
 static void tq_start(char *msg) {
-
+        clock_gettime(CLOCK_REALTIME, &tq_start_time);
 }
 
 /* Stop timing, and print the time since the last tq_start. */
 static void tq_stop(char *msg) {
+        static struct timespec tq_end_time;
+        static long elapsed_s, elapsed_ns;
 
+        clock_gettime(CLOCK_REALTIME, &tq_end_time);
+        elapsed_s = tq_end_time.tv_sec - tq_start_time.tv_sec;
+        elapsed_ns = tq_end_time.tv_nsec - tq_start_time.tv_nsec;
+
+        printf("%s: %lds %ldns\n", msg, elapsed_s, elapsed_ns);
 }
 
 #endif
