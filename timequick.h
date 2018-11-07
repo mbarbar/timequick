e706532d66a598686508b59bb0a7c6fd9f53e486
Mohamad Barbar <mbarbar@runbox.com>
2018-11-07 14:50:30 +1100

    Describe timequick interface.

--- timequick.h
+++ timequick.h
@@ -5,6 +5,7 @@
 #ifndef TIMEQUICK_H
 #define TIMEQUICK_H
 
+/* Time resolution when printing. */
 enum tq_resolution {
         tq_SECONDS,
         tq_MILLISECONDS,
@@ -12,14 +13,17 @@ enum tq_resolution {
         tq_NANOSECONDS
 };
 
+/* Set the time resolution for printing. Only affects output. */
 static void tq_set_resolution(enum tq_resolution resolution) {
 
 }
 
+/* Start timing. */
 static void tq_start(char *msg) {
 
 }
 
+/* Stop timing, and print the time since the last tq_start. */
 static void tq_stop(char *msg) {
 
 }
